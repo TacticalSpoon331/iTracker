e0416b1e8a984eec6a948495bad3710d6e52f3ea
Spoon <47047677+TacticalSpoon331@users.noreply.github.com>
2021-04-25 15:01:28 -0400

    Update parser.py

--- parser.py
+++ parser.py
@@ -1,5 +1,6 @@
 import csv, os, requests
 from collections import Counter
+from progress.bar import Bar
 
 os.system('clear')
 
@@ -10,15 +11,24 @@ API_KEY = 'PUT YOUR POSITIONSTACK REVERSE GEOCODING API KEY HERE'
 
 locations = []
 
+line_count = len(open(csv_data_filename).readlines(  ))
+bar = Bar('Processing location data  ', max=line_count)
+
 with open(csv_data_filename) as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',')
-    for row in csv_reader:
-    	location = requests.get(f'http://api.positionstack.com/v1/reverse?access_key={API_KEY}&query={row[3]},{row[4]}').json()
-    	location = location['data'][0]['name']
-    	print(f'Found location near {row[3]},{row[4]} : {location}')
-    	locations.append(location)
+	csv_reader = csv.reader(csv_file, delimiter=',')
+	for row in csv_reader:
+		try:
+			location = requests.get(f'http://api.positionstack.com/v1/reverse?access_key={API_KEY}&query={row[3]},{row[4]}').json()
+			location = location['data'][0]['name']
+			os.system('clear')
+			bar.next()
+			print('\n')
+			print(f'Found location near {row[3]},{row[4]} : {location}')
 
-
+			locations.append(location)
+		except:
+			pass
+bar.finish()
 
 results = [item for items, c in Counter(locations).most_common()
                                       for item in [items] * c]
